d5933e27e8a57035330111ea19a1b6605dee075c
Zygimantas Gatelis <zygis.gg@gmail.com>
2013-02-08 17:26:07 +0200

    Add method for large matrix generation

--- utilities.py
+++ utilities.py
@@ -10,10 +10,10 @@ from matrix import TridiagonalMatrix, Line
 
 
 def time_function(func, *args, **kwargs):
-	start = time.Clock()
+	start = time.time()
 	ret_val = func(*args, **kwargs)
-	end = time.Clock()
-	return (start-end), ret_val
+	end = time.time()
+	return (end-start), ret_val
 
 def tridiagonal_to_full_matrix_string(initial_matrix):
 	""" Returns string of full matrix. Useful for checking result in matrix calculators
@@ -44,16 +44,15 @@ def tridiagonal_to_compact_matrix_string(initial_matrix):
 	""" Returns string of compact matrix. Matrix size will be n*4
 	"""
 	index = 0
-	matrix = "%.1f %.1f %.1f %.1f\n" % (0, initial_matrix[0].b, initial_matrix[0].c, initial_matrix[0].res)
+	matrix = "%.1f %.1f %.1f\t\t%.1f\n" % (0, initial_matrix[0].b, initial_matrix[0].c, initial_matrix[0].res)
 	for line in initial_matrix[1:-1]:
-		matrix += "%.1f %.1f %.1f %.1f\n" % (line.a, line.b, line.c, line.res)
-	matrix += "%.1f %.1f %.1f %.1f\n" % (initial_matrix[-1].a, initial_matrix[-1].b, 0, initial_matrix[-1].res)
+		matrix += "%.1f %.1f %.1f\t\t%.1f\n" % (line.a, line.b, line.c, line.res)
+	matrix += "%.1f %.1f %.1f\t\t%.1f\n" % (initial_matrix[-1].a, initial_matrix[-1].b, 0, initial_matrix[-1].res)
 	return matrix
 
 def random_tridiagonal_matrix(n=3):
 	""" Returns random tridiagobal matrix as TridiagonalMatrix object
 	"""
-	matrix = []
 	valid = False
 	compare = lambda line: abs(line.b) >= abs(line.a) + abs(line.c)
 	strict_compare = lambda line: abs(line.b) > abs(line.a) + abs(line.c)
@@ -67,18 +66,37 @@ def random_tridiagonal_matrix(n=3):
 			if isValid(line):
 				matrix.append_line(line)
 				valid = True
-			if time.time() - startTime > 5:
-				return
+			if time.time() - startTime > 240:
+				return None
 
 		isValid = compare
 	return matrix
 
-DEFAULT_MATRIX_SIZE = 40
+def silly_tridiagonal_martix(n=3):
+	""" Returns silly tridiagonal matrix. Useful when need very large matrix very fast
+	"""
+	matrix = TridiagonalMatrix()
+	for _ in xrange(n):
+		a = randrange(1, 200)
+		res = randrange(1, 500)
+		line = Line([a, 3*a+1, 2*a, res])
+		matrix.append_line(line)
+	return matrix
+
+
+DEFAULT_MATRIX_SIZE = 10000000
 
 if __name__ == '__main__':
 	n = int(argv[1]) if len(argv) > 1 else DEFAULT_MATRIX_SIZE
 	print "Random %dx%d matrix:" % (n, n)
-	rand_matrix = random_tridiagonal_matrix(n)
-	print tridiagonal_to_full_matrix_string(rand_matrix)
-	print 
-	print tridiagonal_to_compact_matrix_string(rand_matrix)
+	# rand_matrix = random_tridiagonal_matrix(n)
+	rand_matrix = silly_tridiagonal_martix(n)
+	if rand_matrix:
+		# print tridiagonal_to_full_matrix_string(rand_matrix)
+		print 
+		# print tridiagonal_to_compact_matrix_string(rand_matrix)
+		from solver import solve_tridiagonal_matrix
+		time, res = time_function(solve_tridiagonal_matrix, rand_matrix)
+		print "Time to solve:", time
+	else:
+		print "Too less time."
