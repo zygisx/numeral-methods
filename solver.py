f8a9b592ea5697b72683d3eb9af694f538391f3d
Zygimantas Gatelis <zygis.gg@gmail.com>
2013-02-08 10:54:02 +0200

    test stuff removed

--- solver.py
+++ solver.py
@@ -25,29 +25,3 @@ def time_function(func, *args, **kwargs):
 	end = time.Clock()
 	return (start-end), ret_val
 
-
-MATRIX = [
-[0,    2, 0.5,   3],
-[0.5,  2, 0.5,   6],
-[0.5,  2, 0.5,   9],
-[0  ,0.5,   2, 9.5],
-]
-MATRIX2 = [
-	[0., 2., -1.,  1.],
-	[-1., 2., -1., 0.],
-	[-1., 2., 0.,  1.],
-]
-MATRIX3  = [
-	[0.0, -2.16, 1.075,  -0.836],
-	[0.889, -2.16, 1.111, 0.198],
-	[0.834, -2.16, 1.166, 0.441],
-	[0.752, -2.16, 0.0,  -8.238]
-]
-
-matrix = TridiagonalMatrix()
-for line in MATRIX3:
-	matrix.append_line(line)
-
-# for i in matrix:
-	# print i.res
-print [x for x in solve_tridiagonal_matrix(matrix)]
