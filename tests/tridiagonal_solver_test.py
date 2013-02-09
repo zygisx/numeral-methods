f76c6320941ef7779102dc17a455aa97d21a48ee
Zygimantas Gatelis <zygis.gg@gmail.com>
2013-02-09 22:38:05 +0200

    rename tests cases

--- tests/tridiagonal_solver_test.py
+++ tests/tridiagonal_solver_test.py
@@ -3,7 +3,7 @@
 
 import unittest
 from solver import solve_tridiagonal_matrix
-from matrix import TridiagonalMatrix, Line
+from matrix import TridiagonalMatrix
 
 DEFAULT_ACCURACY = 7
 
@@ -27,7 +27,7 @@ class KnownCasesTest(unittest.TestCase):
 		for res, exp in zip(result, expected):
 			self.assertAlmostEqual(res, exp, accuracy)
 
-	def test_simple_matrix(self):
+	def test_3x3_matrix(self):
 		INITIAL_MATRIX = [
 			[0. , 2., -1.,  1.],
 			[-1., 2., -1.,  0.],
@@ -36,7 +36,7 @@ class KnownCasesTest(unittest.TestCase):
 		EXPECTED_RESULT = [1, 1, 1]
 		self.default_tridiagonal_solve_test(INITIAL_MATRIX, EXPECTED_RESULT)
 
-	def test_4_x_4_matrix(self):
+	def test_4x4_matrix(self):
 		INITIAL_MATRIX = [
 			[  0.0, -2.16, 1.075,  -0.836],
 			[0.889, -2.16, 1.111,  0.198],
@@ -46,7 +46,7 @@ class KnownCasesTest(unittest.TestCase):
 		EXPECTED_RESULT = [ 1.51, 2.257, 3.357, 4.983]
 		self.default_tridiagonal_solve_test(INITIAL_MATRIX, EXPECTED_RESULT, 2)
 
-	def test_9_x_9_matrix(self):
+	def test_9x9_matrix(self):
 		INITIAL_MATRIX = [
 			[0.0, -3.0, -1.0, -5.0],
 			[2.0, 5.0, -2.0, 5.0],
@@ -71,7 +71,7 @@ class KnownCasesTest(unittest.TestCase):
 		]
 		self.default_tridiagonal_solve_test(INITIAL_MATRIX, EXPECTED_RESULT, 3)
 
-	def test_12_x_12_matrix(self):
+	def test_12x12_matrix(self):
 		INITIAL_MATRIX = [ 
 			[0.0 , 27.0, 5.0 , 21.0],
 			[1.0 , 41.0, 28.0, 38.0],
@@ -102,7 +102,7 @@ class KnownCasesTest(unittest.TestCase):
 		]
 		self.default_tridiagonal_solve_test(INITIAL_MATRIX, EXPECTED_RESULT, 6)
 
-	def test_20_x_20_matrix(self):
+	def test_20x20_matrix(self):
 		INITIAL_MATRIX = [ 
 			[0.0 , 40.0, 8.0 , 49.0],
 			[16.0, 42.0, 6.0 , 45.0],
