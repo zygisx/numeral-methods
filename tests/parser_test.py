ed5ea893e0eec5b8076e8191b46d20a83b984256
Zygimantas Gatelis <zygis.gg@gmail.com>
2013-02-16 16:33:33 +0200

    implemented parser with tests cases

--- tests/parser_test.py
+++ tests/parser_test.py
@@ -5,27 +5,90 @@ import unittest
 import StringIO
 
 from matrix import TridiagonalMatrix
-from matrix_parser import MatrixParser
+from matrix_parser import TridiagonalMatrixParser
 
 
-class TridiagonalParserTest(unittest.TestCase):
+class ParserTest(unittest.TestCase):
 	""" class to test parser module
 	"""
 
 	def setUp(self):
-		self.parser = MatrixParser()
+		self.parser = TridiagonalMatrixParser()
 
-	def test_empty_file(self):
-		pass
+	def test_doc_parse(self):
+		file_obj = StringIO.StringIO()
 
-	def test_only_comments_file_parse(self):
-		pass
+		#tridiagonal parse
+		matrix = self.parser.parse_from_tridiagonal_matrix(file_obj)
+		self.assertEqual(len(matrix), 0)
+		self.assertEqual(matrix.description, "")
 
-	def test_only_matrix_parse(self):
-		pass
+		file_obj = StringIO.StringIO()
 
-	def test_matrix_and_comments_parse(self):
-		pass
+		# from full matrix
+		matrix = self.parser.parse_from_full_matrix(file_obj)
+		self.assertEqual(len(matrix), 0)
+		self.assertEqual(matrix.description, "")
+
+
+	def test_doc_parse_tridiagonal(self):
+		file_obj = StringIO.StringIO()
+		file_obj.write("#First line.\n")
+		file_obj.write("#Second line.\n")
+		file_obj.seek(0)
+
+		#tridiagonal parse
+		matrix = self.parser.parse_from_tridiagonal_matrix(file_obj)
+		self.assertEqual(len(matrix), 0)
+		self.assertEqual(matrix.description, "First line.\nSecond line.\n")
+
+	def test_doc_parse_full_matrix(self):
+		file_obj = StringIO.StringIO()
+		file_obj.write("#First line.\n")
+		file_obj.write("#Second line.\n")
+		file_obj.seek(0)
+
+		# from full matrix
+		matrix = self.parser.parse_from_full_matrix(file_obj)
+		self.assertEqual(len(matrix), 0)
+		self.assertEqual(matrix.description, "First line.\nSecond line.\n")
+
+	def test_tridiagonal_matrix_parse(self):
+		file_obj = StringIO.StringIO()
+		file_obj.write("0 1. 2.0 3.0\n")
+		file_obj.write("4.0 5. 6. 7.\n")
+		file_obj.write("8. 9. 10. 11\n")
+		file_obj.write("12 13. 0.0 15.0\n")
+		file_obj.seek(0)
+
+		#tridiagonal parse
+		matrix = self.parser.parse_from_tridiagonal_matrix(file_obj)
+
+		self.assertEqual(matrix[0].line, [0., 1., 2., 3.])
+		self.assertEqual(matrix[1].line, [4, 5, 6, 7])
+		self.assertEqual(matrix[2].line, [8, 9, 10, 11])
+		self.assertEqual(matrix[3].line, [12, 13, 0, 15])
+		self.assertEqual(matrix.description, "")
+
+
+
+	def test_full_matrix_parse(self):
+		file_obj = StringIO.StringIO()
+		file_obj.write("1.  2.  0.  0.  3.0 \n")
+		file_obj.write("4.0 5.  6.  0.  7.  \n")
+		file_obj.write("0.  8.  9.  10. 11. \n")
+		file_obj.write("0.  0.0 12. 13. 15.0\n")
+		file_obj.seek(0)
+
+		#tridiagonal parse
+		matrix = self.parser.parse_from_full_matrix(file_obj)
+
+		self.assertEqual(len(matrix), 4)
+		self.assertEqual(matrix[0].line, [0, 1, 2, 3])
+		self.assertEqual(matrix[1].line, [4, 5, 6, 7])
+		self.assertEqual(matrix[2].line, [8, 9, 10, 11])
+		self.assertEqual(matrix[3].line, [12, 13, 0, 15])
+		self.assertEqual(matrix.description, "")
 
 
 
